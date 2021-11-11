4b413b04bc862e699e1ce97ebadfd75d02235233
Anna Manowski <anna.l.manowski@gmail.com>
2021-11-11 10:56:13 +0100

    Add jQuery to add new Item

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function newItem() {
+    //1. Adding a new item to the list of items:
+    const li = $('<li></li>');
+    const inputValue = $('#input').val();
 
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        $('#list').append(li);
+    }
 }
